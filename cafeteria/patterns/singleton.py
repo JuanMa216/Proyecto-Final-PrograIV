7cd2d76dfe7101841184acdb3c6289e743ba6211
JulRiv07 <Julian.rivera@utp.edu.co>
2026-05-17 16:42:05 -0500

    Cambio de modularidad

--- cafeteria/patterns/singleton.py
+++
@@ -1,6 +0,0 @@
-# patterns/singleton.py
-# Patron: Singleton (re-export)
-#
-# TODO: Re-exportar DatabaseConnection.get_instance()
-#   - TODO: Importar DatabaseConnection desde database.connection
-#   - TODO: Exportar alias db = DatabaseConnection.get_instance()
