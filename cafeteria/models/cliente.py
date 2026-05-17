db243297870e7e187d3b325de2eed0175f0ccbe6
JuanMa216 <juan.rojas6@utp.edu.co>
2026-05-16 22:05:46 -0500

    Implementacion de los modelos dentro de models/, ya estan listas las clases basicas para cada uno. Tambien se implementó el patron factory para tener un codigo y ejecucion mas limpio

--- cafeteria/models/cliente.py
+++ cafeteria/models/cliente.py
@@ -5,3 +5,33 @@
 #   - TODO: Atributos: id, nombre, puntos_fidelidad, es_frecuente
 #   - TODO: Metodo acumular_puntos(monto) que sume puntos segun el monto
 #   - TODO: Metodo canjear_puntos(puntos) que reste puntos disponibles
+from dataclasses import dataclass, field
+from models.persona import Persona
+
+@dataclass 
+class Cliente(Persona):
+    _id_cliente: int
+    _puntos_fidelidad: int = 0
+
+    @property
+    def id_cliente(self):
+        return self._id_cliente
+
+    def puntos_fidelidad(self):
+        return self._puntos_fidelidad
+    
+    def get_rol(self) -> str:
+        return "Cliente"
+
+    def agregar_puntos(self, puntos: int):
+        if puntos < 0:
+            raise ValueError("Los puntos no pueden ser negativos")
+        self._puntos_fidelidad += puntos
+
+    def canjear_puntos(self, puntos: int):
+        if puntos > self._puntos_fidelidad:
+            raise ValueError("Puntos insuficientes") 
+        self._puntos_fidelidad -= puntos
+
+    def __str__(self):
+        return f"{self._nombre} {self._apellido} | Puntos: {self._puntos_fidelidad}"
--- cafeteria/models/producto.py
+++ cafeteria/models/producto.py
@@ -5,3 +5,47 @@
 #   - TODO: Atributos: id, nombre, precio_base, categoria, descripcion
 #   - TODO: Metodo calcular_precio() que retorne el precio_base
 #   - TODO: Metodo __str__() con representacion del producto
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+@dataclass 
+class Producto(ABC):
+    _nombre: str
+    _precio: float
+    _categoria: str
+    _descripcion: str
+    _disponible: bool
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @property
+    def precio(self):
+        return self._precio
+
+    @property
+    def descripcion(self):
+        return self._descripcion
+
+    @property 
+    def disponible(self):
+        return self._disponible
+
+    @precio.setter
+    def precio(self, nuevo):
+        if nuevo < 0:
+            raise ValueError("El precio no puede ser negativo")
+        self._precio = nuevo
+
+    @disponible.setter
+    def disponible(self, valor: bool):
+        self._disponible = valor
+
+    @abstractmethod
+    def preparar(self) -> str:
+        pass
+
+    def __str__(self):
+        estado = "Disponible" if self._disponible else "No disponible"
+        return f"{self._nombre} - ${self._precio:,.0f} ({estado})" 
