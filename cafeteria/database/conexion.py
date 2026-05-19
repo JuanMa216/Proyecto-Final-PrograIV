b7c5641b6e390fb21930889f84f07f5c6dab1596
JulRiv07 <Julian.rivera@utp.edu.co>
2026-05-18 23:57:28 -0500

    Refactoriza flujo del sistema eliminando servicio de ventas y tests; mejora captura y validación de datos

--- cafeteria/database/conexion.py
+++ cafeteria/database/conexion.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from database.config import DATABASE_URL
 
+
 engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
